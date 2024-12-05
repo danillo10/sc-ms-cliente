20b90e9581e2eee3911fc61d4854f97179c38ca7
Danillo Leão Deucher <danillo_10_@hotmail.com>
2024-12-05 10:13:21 -0300

    Mudança de nome do microserviço

--- ms-cliente/src/test/java/com/c9tecnologia/cliente/ClienteApplicationTests.java
+++ ms-cliente/src/test/java/com/c9tecnologia/cliente/ClienteApplicationTests.java
@@ -6,7 +6,7 @@ import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
-class ComercialApplicationTests {
+class ClienteApplicationTests {
 
 	@Test
 	void contextLoads() {
